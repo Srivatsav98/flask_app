ce5ecfd71f393e2435f1051e79045fdef3fb90bf
Srivatsav98 <srivatsav.yechiraju98@gmail.com>
2023-02-05 11:09:51 -0500

    added

--- project/flaskapp.py
+++ project/flaskapp.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_login import LoginManager
-from flask_sqlalchemy import SQLAlchemy
+
 
 # init SQLAlchemy so we can use it later in our models
 from models import db
--- project/models.py
+++ project/models.py
@@ -1,4 +1,4 @@
-
+from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin
 db = SQLAlchemy()
 class User(UserMixin,db.Model):
